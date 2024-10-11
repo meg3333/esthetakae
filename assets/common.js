e3f483df9bfef2ef23ab2b3d6088f58b3ea4b376
Megumi Ikeda <1gk2318i@gmail.com>
2024-10-11 21:12:08 +0900

    first commit

---
+++ assets/common.js
@@ -0,0 +1,7 @@
+// JavaScript Document
+$(function () {
+  $('#js-hamburger-menu, .navigation__link').on('click', function () {
+    $('.navigation').slideToggle(500)
+    $('.hamburger-menu').toggleClass('hamburger-menu--open')
+  });
+});
